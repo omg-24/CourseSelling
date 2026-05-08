e7c4150b20a93f8b5735517c56012a647138b393
Om Gaurav <gauravom84@gmail.com>
2026-05-08 20:08:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
+const app = express()
+
+
+app.use(express.json())
+
+app.post('signup',async function (req,res) {
+    
+})
+
+
+app.get('signin',async function (req,res) {
+    
+})
+
+app.post('purchase',auth, (req,res)=>{
+
+})
+
+app.get('course',auth, (req,res)=>{
+
+})
+
+app.get('allcourse', (req,res)=>{
+
+})
